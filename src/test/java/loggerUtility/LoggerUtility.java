9b41e1935402e0d995839e7cdceca23a783ef493
AlexDorha <alex_dorha@yahoo.com>
2024-10-21 21:23:15 +0300

    implement nightly pipeline

--- src/test/java/loggerUtility/LoggerUtility.java
+++ src/test/java/loggerUtility/LoggerUtility.java
@@ -4,11 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.PrintWriter;
-
 public class LoggerUtility {
     private static final Logger logger = LogManager.getLogger();
 
